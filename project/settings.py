6947cae74c769c60eef1d9687cc2ab5f418e26c6
Leandro Carriego <leandrocarriego@hotmail.com>
2024-10-16 11:31:26 -0300

    feat: add staticfiles urls

--- project/settings.py
+++ project/settings.py
@@ -122,6 +122,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, "public/static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "public/media")
+
 # Default primary key field type ---------------------------------------------------------------------------------------
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
